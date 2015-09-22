3ae2c1c61d262dc7ece62f05e3cdf73599822d9c
SillyFreak <sillyfreak.ck@gmail.com>
2015-09-22 09:42:44 +0200

    add rcc module - a little stub-y

--- src/stm32f3/mod.rs
+++ src/stm32f3/mod.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
 
 pub mod gpio;
+pub mod rcc;
 
---
+++ src/stm32f3/rcc.rs
@@ -0,0 +1,43 @@
+#[repr(C, packed)]
+#[allow(non_snake_case)]
+pub struct Rcc {
+    pub CR:       u32, //0x00
+    pub CFGR:     u32, //0x04
+    pub CIR:      u32, //0x08
+    pub APB2RSTR: u32, //0x0C
+    pub APB1RSTR: u32, //0x10
+    pub AHBENR:   u32, //0x14
+    pub APB2ENR:  u32, //0x18
+    pub APB1ENR:  u32, //0x1C
+    pub BDCR:     u32, //0x20
+    pub CSR:      u32, //0x24
+    pub AHBRSTR:  u32, //0x28
+    pub CFGR2:    u32, //0x2C
+    pub CFGR3:    u32, //0x30
+}
+
+registers! {
+    registers: Rcc {
+        const RCC = 0x40021000,
+    }
+}
+
+bitflags! {
+    flags AHBENR: u32 {
+        const DMA1EN  = 1 <<  0,
+        const DMA2EN  = 1 <<  1,
+        const SRAMEN  = 1 <<  2,
+        const FLITFEN = 1 <<  4,
+        const CRCEN   = 1 <<  6,
+        const GPIOAEN = 1 << 17,
+        const GPIOBEN = 1 << 18,
+        const GPIOCEN = 1 << 19,
+        const GPIODEN = 1 << 20,
+        const GPIOEEN = 1 << 21,
+        const GPIOFEN = 1 << 22,
+        const TSEN    = 1 << 24,
+        const ADC12EN = 1 << 28,
+        const ADC34EN = 1 << 29,
+    }
+}
+
