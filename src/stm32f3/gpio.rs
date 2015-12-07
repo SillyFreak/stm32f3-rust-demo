ac5d3a984d2c13f6ea56ef59dd66209c85105364
SillyFreak <sillyfreak.ck@gmail.com>
2015-12-07 20:46:26 +0100

    correct Port struct definition

--- src/stm32f3/gpio.rs
+++ src/stm32f3/gpio.rs
@@ -3,20 +3,20 @@ use registers::RegPtr;
 #[repr(C, packed)]
 #[allow(non_snake_case)]
 pub struct Port {
-    pub MODER:     u32, //0x00
-    pub OTYPER:    u16, //0x04
-    pub RESERVED0: u16, //0x06
-    pub OSPEEDR:   u32, //0x08
-    pub PUPDR:     u32, //0x0C
-    pub IDR:       u16, //0x10
-    pub RESERVED1: u16, //0x12
-    pub ODR:       u16, //0x14
-    pub RESERVED2: u16, //0x16
-    pub BSRR:      u32, //0x18
-    pub LCKR:      u32, //0x1C
-    pub AFR:       u32, //0x20-0x24
-    pub BRR:       u16, //0x28
-    pub RESERVED3: u16, //0x2A
+    pub MODER:     u32,      //0x00
+    pub OTYPER:    u16,      //0x04
+    pub RESERVED0: u16,      //0x06
+    pub OSPEEDR:   u32,      //0x08
+    pub PUPDR:     u32,      //0x0C
+    pub IDR:       u16,      //0x10
+    pub RESERVED1: u16,      //0x12
+    pub ODR:       u16,      //0x14
+    pub RESERVED2: u16,      //0x16
+    pub BSRR:      u32,      //0x18
+    pub LCKR:      u32,      //0x1C
+    pub AFR:       [u32; 2], //0x20-0x24
+    pub BRR:       u16,      //0x28
+    pub RESERVED3: u16,      //0x2A
 }
 
 registers! {
